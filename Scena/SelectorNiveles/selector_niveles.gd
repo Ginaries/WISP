29aad95a407df298d2dad1ff4d31b1efafecced7
Luciana1007 <luliandrea231@gmail.com>
2025-10-28 01:59:03 -0300

    Cambios y nivel 1 completado
    Se realizó un cambió visual al menú y al selector de niveles. Además se terminó el mapeado del nivel 1 con la colocación de braseros y algunos enemigos.

--- Scena/SelectorNiveles/selector_niveles.gd
+++ Scena/SelectorNiveles/selector_niveles.gd
@@ -1,8 +1,8 @@
 extends Control
 
-
 func _on_nivel_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scena/TilesSet/TilesSet.tscn")
+
 
 func _on_nivel_2_pressed() -> void:
 	pass # Replace with function body.
