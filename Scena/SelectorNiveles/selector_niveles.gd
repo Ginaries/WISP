cc4e05179b30601f04324b0c1f7c047508bba7b6
Luciana1007 <luliandrea231@gmail.com>
2025-11-03 22:07:17 -0300

    Actualización?

--- Scena/SelectorNiveles/selector_niveles.gd
+++ Scena/SelectorNiveles/selector_niveles.gd
@@ -18,3 +18,7 @@ func _on_nivel_4_pressed() -> void:
 
 func _on_nivel_5_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_tienda_premium_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scena/TiendaPremium/TiendaPremium.tscn")
--- Script/Menú/menu.gd
+++ Script/Menú/menu.gd
@@ -5,7 +5,7 @@ func _on_jugar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scena/SelectorNiveles/SelectorNiveles.tscn")
 
 func _on_tienda_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scena/TiendaPremium/TiendaPremium.tscn")
 
 
 func _on_controles_pressed() -> void:
